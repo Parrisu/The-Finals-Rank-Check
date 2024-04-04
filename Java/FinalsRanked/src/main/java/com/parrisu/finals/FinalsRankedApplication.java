bc426d5a5a1553ebd42790075d53c5eac4a03984
Parrisu <parris.a.jones@gmail.com>
2024-04-04 09:13:46 -0700

    initialize Spring Boot

---
+++ Java/FinalsRanked/src/main/java/com/parrisu/finals/FinalsRankedApplication.java
@@ -0,0 +1,13 @@
+package com.parrisu.finals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalsRankedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalsRankedApplication.class, args);
+	}
+
+}
---
+++ Java/FinalsRanked/src/test/java/com/parrisu/finals/FinalsRankedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parrisu.finals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalsRankedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
